7536c2830f011b35bfb02140cb0ee248cc8fcce5
akain88 <larahaze88@gmail.com>
2019-10-14 12:11:55 -0500

    switch excercise with loops finish

--- src/com/practica5switch/app/Switchstatement.java
+++ src/com/practica5switch/app/Switchstatement.java
@@ -1,10 +1,67 @@
 package com.practica5switch.app;
 
+import java.util.Scanner;
+
 public class Switchstatement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//variable declaration
+		
+		//variable declaration
+	int nMes=0;
+	char answer='s';
+	
+	//object constructruction
+	
+		Scanner input = new Scanner(System.in);
+		do
+		{
+			
+		
+		
+		//input data validation
+		while(nMes<=0)
+		{
+		System.out.println("gime me a month day");
+		nMes =input.nextInt();
+        if (nMes<=0) 
+        {
+        	System.out.println("Dato No Valido");}
 	}
-
+	
+     switch (nMes) {
+     case 1: System.out.println("january");
+     break;
+     case 2: System.out.println("february");
+     break;
+     case 3: System.out.println("marse");
+     break;
+     case 4: System.out.println("april");
+     break;
+     case 5: System.out.println("may");
+     break;
+     case 6: System.out.println("jane");
+     break;
+     case 7: System.out.println("july");
+     break;
+     case 8: System.out.println("august");
+     break;
+     case 9: System.out.println("september");
+     break;
+     case 10: System.out.println("october");
+     break;
+     case 11: System.out.println("november");
+     break;
+     case 12: System.out.println("december");
+     break;
+     default: System.out.println("Not Valid");
+     break;
+     }
+     System.out.println("do yoiu wish to see another month?(y/n))");
+     }while (answer=='s');
+		
+     answer=input.next().charAt(0);
+		
+	
+		}
 }
